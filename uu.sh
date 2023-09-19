80c7eac96490a2e71c0379b63104080c7e097da3
Athena Shikata <99115384+AthenaShikata@users.noreply.github.com>
2023-09-19 15:31:18 -0700

    Update uu.sh

--- uu.sh
+++ uu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade -y
-if sudo apt-get upgrade | grep -q 'sudo apt-get autoremove'
+if sudo apt-get upgrade | grep -q 'sudo apt autoremove'
 then
   sudo apt-get autoremove -y
 fi
